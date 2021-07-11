4e2fa4b4e2960d95a3da5e1b67c0e307c0d9d1ba
rink2616 <ranarinkeshit@gmail.com>
2021-07-11 13:36:45 +0530

    american guy commit

--- src/test/java/qaclickacademy/RestAPITest.java
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -9,9 +9,13 @@ public class RestAPITest {
 	public void PostJira() 
 	{
 		System.out.println("PostJira");
+		
 		System.out.println("PostJira2");
 		System.out.println("PostJira3");
-		
+		System.out.println("PostJira4");
+		System.out.println("PostJira5");
+		System.out.println("PostJira6");
+
 	}
 	@Test
 	public void DeleteTwitter() 

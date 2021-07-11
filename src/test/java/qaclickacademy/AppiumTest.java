37fee449b8cea3c812a7505ce075a4f6e3423830
rink2616 <ranarinkeshit@gmail.com>
2021-07-11 12:16:34 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeappAndroid() 
+	{
+		System.out.println("NativeappAndroid");
+	}
+	@Test
+	public void Iosapps() 
+	{
+		System.out.println("IOSapps");
+	}
+}
